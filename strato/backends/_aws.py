c93295eca996b01075b5a572a8314fc1769cf05d
Yiming Yang <yang.yihming@gmail.com>
2021-08-23 01:09:13 -0400

    fix bugs when testing

--- strato/backends/_aws.py
+++ strato/backends/_aws.py
@@ -1,3 +1,4 @@
+import os
 from subprocess import check_call
 
 
@@ -6,11 +7,9 @@ class AWSBackend:
         self._backend = 'aws'
         self._call_prefix = ['aws', 's3']
 
-    def copy(self, recursive, filenames):
+    def copy(self, filenames):
         call_args = self._call_prefix.copy()
         call_args.extend(['cp', '--only-show-errors'])
-        if recursive:
-            call_args.append('--recursive')
 
         source_files = filenames[:-1]
         target = filenames[-1]
@@ -20,30 +19,37 @@ class AWSBackend:
         # Copy files one by one.
         for source in source_files:
             subcall_args = call_args.copy()
-            subcall_args.extend([source, target])
+            subcall_target = target
+            if source[-1] == '/':
+                subcall_args.append('--recursive')
+                if subcall_target[-1] != '/':
+                    subcall_target = subcall_target + '/' + os.path.basename(source)
+                else:
+                    subcall_target = subcall_target + os.path.basename(source)
+            subcall_args.extend([source, subcall_target])
             print(' '.join(subcall_args))
             check_call(subcall_args)
 
     def sync(self, source, target):
         call_args = self._call_prefix.copy()
-        call_args.extend(['sync', '--delete', source, target])
+        call_args.extend(['sync', '--delete', '--only-show-errors', source, target])
         print(' '.join(call_args))
         check_call(call_args)
 
-    def delete(self, recursive, filenames):
+    def delete(self, filenames):
         call_args = self._call_prefix.copy()
-        call_args.extend(['rm', '--quiet'])
-        if recursive:
-            call_args.append('--recursive')
+        call_args.extend(['rm', '--only-show-errors'])
 
         # Delete files one by one.
         for f in filenames:
             subcall_args = call_args.copy()
+            if f[-1] == '/':
+                subcall_args.append('--recursive')
             subcall_args.append(f)
             print(' '.join(subcall_args))
             check_call(subcall_args)
 
     def stat(self, filename):
         call_args = self._call_prefix.copy()
-        call_args.extend(['ls', filename])
+        call_args.extend(['cp', '--quiet', '--dryrun', filename, 'null'])
         check_call(call_args)
