35c3f48645bf97916d2187e1cbe9b2797b8d75de
Yiming Yang <yang.yihming@gmail.com>
2021-11-08 13:21:04 -0500

    add version check command

--- strato/__main__.py
+++ strato/__main__.py
@@ -1,6 +1,10 @@
 import sys
 from strato.commands import *
 
+try:
+    from importlib.metadata import version
+except ImportError:  # < Python 3.8: Use backport module
+    from importlib_metadata import version
 
 def main():
     command_list = [cp, sync, rm, exists]
@@ -8,6 +12,7 @@ def main():
     command_list_strings = list(map(lambda c: c.__name__[len('strato.commands.'):], command_list))
     parser.add_argument('command', help='The command', choices=command_list_strings)
     parser.add_argument('command_args', help='The command arguments', nargs=argparse.REMAINDER)
+    parser.add_argument('-v', '--version', action="version", version=version('stratocumulus'))
 
     my_args = parser.parse_args()
     command_name = my_args.command
